81432c4fc3fcd85c70f3b2bf874a9594d974accf
mdave94 <molnardavid2002@gmail.com>
2023-09-14 13:22:42 +0200

    footer update

--- src/screens/Mainpage.tsx
+++ src/screens/Mainpage.tsx
@@ -7,22 +7,23 @@ import dividerPict from "../assets/img/image02.jpg";
 import twoItemPic from "../assets/img/image07.jpg";
 import MainpageTextComponent from "../components/mainpage/MainpageTextComponent";
 import Footer from "../components/FooterComponent";
+
 export default function App() {
   const parallax = useRef<IParallax>(null!);
 
   return (
-    <div style={{ width: "100%", height: "100%", background: "#253237" }}>
-      <Parallax ref={parallax} pages={2.5}>
-        <ParallaxLayer offset={0} speed={0}>
-          <div
-            style={{
-              backgroundImage: `url(${mainPagePicture})`,
-              backgroundSize: "cover",
-              height: 1100,
-            }}
-          />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.1} speed={1}>
+    <Parallax ref={parallax} pages={3} style={{ backgroundColor: "#AE9A90" }}>
+      <ParallaxLayer offset={0} speed={0}>
+        <div
+          style={{
+            backgroundImage: `url(${mainPagePicture})`,
+            backgroundSize: "cover",
+            minHeight: "100vh", // Set a minimum height to fill the viewport
+          }}
+        />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.1} speed={1}>
+        <div>
           <img
             src={minnaeLogoText}
             style={{
@@ -31,46 +32,48 @@ export default function App() {
             }}
           />
           <p style={{ fontSize: 24 }}>Fedezze fel a mosolyékszerek világát</p>
-        </ParallaxLayer>
+        </div>
+      </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={0.9}
-          speed={1}
-          style={{
-            width: "100%",
-            height: "110vh",
-            backgroundColor: "rgb(174, 154, 144)",
-          }}
-        >
-          <div className="mainpage-container">
-            <p className="text-title">Mi is az a Mosolyékszer</p>
+      <ParallaxLayer
+        offset={0.9}
+        speed={1}
+        style={{
+          width: "100%",
+          height: "110vh",
+          backgroundColor: "rgb(174, 154, 144)",
+        }}
+      >
+        <div className="mainpage-container">
+          <p className="text-title">Mi is az a Mosolyékszer</p>
 
-            <MainpageTextComponent>
-              A hagyományos fogékszerektől eltérően mosolyékszereink egyszerűen
-              használhatóak, könnyen behelyezhetőek és eltávolíthatók a szájból.
-            </MainpageTextComponent>
-            <div className="alert-textbox">
-              NEM IGÉNYELNEK FOGÁSZATI BEAVATKOZÁST SEM FOGORVOS KÖZREMŰKÖDÉSÉT
-            </div>
-            <MainpageTextComponent>
-              Behelyezésükhöz nem szükséges megbontani az egészséges fogzománc
-              egységét sem fúrással, sem savas felület előkezeléssel.
-            </MainpageTextComponent>
-
-            <div className="alert-textbox">
-              MEGFELELŐ HASZNÁLAT MELLET TEHÁT TELJESEN BIZTOSÁGOS ESZKÖSZ
-            </div>
-            <MainpageTextComponent>
-              Kialakításuknak köszönhetően stabilan helyben maradnak, ugyanakkor
-              nem zavarják a fogak érintkezését.
-            </MainpageTextComponent>
-            <div className="alert-textbox">
-              Ennek eredményeként nem kell a helybentartásukra koncentrálni
-            </div>
+          <MainpageTextComponent>
+            A hagyományos fogékszerektől eltérően mosolyékszereink egyszerűen
+            használhatóak, könnyen behelyezhetőek és eltávolíthatók a szájból.
+          </MainpageTextComponent>
+          <div className="alert-textbox">
+            NEM IGÉNYELNEK FOGÁSZATI BEAVATKOZÁST SEM FOGORVOS KÖZREMŰKÖDÉSÉT
           </div>
-        </ParallaxLayer>
+          <MainpageTextComponent>
+            Behelyezésükhöz nem szükséges megbontani az egészséges fogzománc
+            egységét sem fúrással, sem savas felület előkezeléssel.
+          </MainpageTextComponent>
 
-        <ParallaxLayer offset={1} speed={1}>
+          <div className="alert-textbox">
+            MEGFELELŐ HASZNÁLAT MELLET TEHÁT TELJESEN BIZTOSÁGOS ESZKÖSZ
+          </div>
+          <MainpageTextComponent>
+            Kialakításuknak köszönhetően stabilan helyben maradnak, ugyanakkor
+            nem zavarják a fogak érintkezését.
+          </MainpageTextComponent>
+          <div className="alert-textbox">
+            Ennek eredményeként nem kell a helybentartásukra koncentrálni
+          </div>
+        </div>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={1} speed={1}>
+        <div>
           <img
             src={dividerPict}
             style={{
@@ -79,34 +82,34 @@ export default function App() {
               boxShadow: "10px 10px 60px black",
             }}
           />
-        </ParallaxLayer>
+        </div>
+      </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={1.3}
-          speed={1}
-          style={{ backgroundColor: "#ae9a90", height: 1100, zIndex: 0 }}
-        >
-          <p className="text-title">Vásárlás menete</p>
+      <ParallaxLayer
+        offset={1.3}
+        speed={1}
+        style={{ backgroundColor: "#ae9a90", height: 800, zIndex: 0 }}
+      >
+        <p className="text-title">Vásárlás menete</p>
 
-          <MainpageTextComponent>
-            Az előzetesen lefoglalt időpontban várjuk Önt központunkban. Mivel
-            minden ékszerünk tökéletesen személyre szabott, a tervezés közösen
-            történik. Kiválasztjuk a díszlap formáját, meretét, majd a
-            díszítését. Ezután meghatározzuk az átmenő stift méretét. Végül
-            felvesszük rendelését.
-          </MainpageTextComponent>
-          <MainpageTextComponent>
-            Ékszerészünk elkészíti a közösen megálmodott mosolyékszerét.
-          </MainpageTextComponent>
-          <MainpageTextComponent>
-            Az elkészült ékszert kiszállítjuk Önnek.
-          </MainpageTextComponent>
-        </ParallaxLayer>
+        <MainpageTextComponent>
+          Az előzetesen lefoglalt időpontban várjuk Önt központunkban. Mivel
+          minden ékszerünk tökéletesen személyre szabott, a tervezés közösen
+          történik. Kiválasztjuk a díszlap formáját, meretét, majd a díszítését.
+          Ezután meghatározzuk az átmenő stift méretét. Végül felvesszük
+          rendelését.
+        </MainpageTextComponent>
+        <MainpageTextComponent>
+          Ékszerészünk elkészíti a közösen megálmodott mosolyékszerét.
+        </MainpageTextComponent>
+        <MainpageTextComponent>
+          Az elkészült ékszert kiszállítjuk Önnek.
+        </MainpageTextComponent>
+      </ParallaxLayer>
 
-        <ParallaxLayer offset={-1} speed={-1.2} style={{ zIndex: -1 }}>
-          <Footer />
-        </ParallaxLayer>
-      </Parallax>
-    </div>
+      <ParallaxLayer offset={-1} speed={-1.2} style={{ zIndex: -1 }}>
+        <Footer />
+      </ParallaxLayer>
+    </Parallax>
   );
 }
